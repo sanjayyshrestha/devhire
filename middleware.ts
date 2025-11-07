071946e34ea41e82a212103687d6f8df308e0637
sanjayshrestha <sanjayshrestha772@gmail.com>
2025-11-07 19:00:19 +0545

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth"
+
+export default auth
 
 export const config = {
   matcher: ["/profile-setup/:path*", "/select-role/:path*"],
-  runtime:'nodejs'
-};
+}
